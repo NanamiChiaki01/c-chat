67090b50f463a94127cbe342d7288aa0e200d83e
701-extreme <evenlyrl@gmail.com>
2024-12-09 16:27:16 +1300

    testing

--- server.c
+++ server.c
@@ -1,3 +1,49 @@
-#include <server.h>
+#include "server.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+// Server constructor
+struct Server server_constructor(int domain, int service, int protocol, u_long interface, int port, int backlog, void(*launch)(void))
+{
+  struct Server server;
 
+  server.domain = domain;
+  server.service = service;
+  server.protocol = protocol;
+  server.interface = interface;
+  server.port = port;
+  server.backlog =  backlog;
+  server.launch = launch;
+  
+  server.address.sin_family = domain;
+  server.address.sin_port = hton(port); // convert digital port to meaningful network port
+  server.address.sin_addr.s_addr = htol(interface); // convert to long interface to the network
+
+  // create socket connection to the network
+  server.socket = socket(domain, service, protocol);
+  if (server.socket == 0)
+  {
+    perror("Failed to connect socket...\n");
+    exit(1);
+  }
+
+  // need to cast the socket address created to a sockaddr pointer for reference
+  if(bind(server.socket, (struct sockaddr*)&server.address, sizeof(server.address))< 0 )
+  {
+    perror("Failed to bind socket...\n");
+    exit(1);
+  }
+
+  // tell the server that it should be listening
+  // the backlog defines how many connections are allowed to be waiting
+  // this will check error
+  if(listen(server.socket, int server.backlog)<0)
+  {
+    perror("Failed to listen ... \n");
+    exit(1);
+  }
+
+  server.launch = launch;
+
+  return server;
+}
--- server.h
+++ server.h
@@ -1,19 +1,27 @@
 #ifndef SERVH
 #define SERVH
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 struct Server
 {
   int domain;
   int service;
   int protocol;
   u_long interface;
-  int port;
+  int port
   int backlog;
 
   struct sockaddr_in address;
 
-  void (*launch) void; // for the user to decide which service should it launch
-                      
-}
+  int socket;
+
+  // some function that should take no param, for user to call
+  void (*launch)(void); // for the user to decide which service should it launch
+};
+
+// passing in all these to construct a server fot the users
+struct Server server_constructor(int domain, int service, int protocol, u_long interface, int port, int backlog, void(*launch)(void));
 
 #endif
