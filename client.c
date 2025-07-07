4fe243f5c4937b8aeaec86dcedc3cd9cb1c5bf6e
701 <evenlyrl@gmail.com>
2025-07-07 17:11:40 +1000

    rebuilding with new structure

--- client.c
+++ client.c
@@ -1,50 +1,71 @@
-// Client code in C
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <arpa/inet.h>
 
-#define PORT 8080
+#define PORT 12345
 #define BUFFER_SIZE 1024
 
-int client() {
-    int sock = 0;
-    struct sockaddr_in serv_addr;
+// Global variables
+int sock; 
 
-    // Creating socket
+// function prototypes
+void *receive_messages(void* arg);
+
+int main() {
+    struct sockaddr_in serv_addr;
+    char message[BUFFER_SIZE] = {0};
+    pthread_t recv_thread;
+
+    // 1. Create socket
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("Socket creation error");
         exit(EXIT_FAILURE);
     }
 
+    // 2. Setup server address
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    // Convert IPv4 and IPv6 addresses from text to binary form
+    // 3. Convert IP
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        perror("Invalid address/ Address not supported");
+        perror("Invalid address");
         exit(EXIT_FAILURE);
     }
 
-    // Connecting to server
+    // 4. Connect
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
         perror("Connection failed");
         exit(EXIT_FAILURE);
     }
-  return sock;
+
+    printf("Connected to chatroom.\n");
+    pthread_create(&recv_thread, NULL, receive_messages, NULL);
+
+    // 5. Communicate
+    while (1) {
+        printf("> ");
+        fgets(message, BUFFER_SIZE, stdin);
+        send(sock, message, strlen(message), 0);
+    }
+
+    close(sock);
+    return 0;
 }
 
-void send_msg(int sock){
-  /*prompt for input and loop*/
-    char *message = "Hello from client";
+void *receive_messages(void *arg) {
     char buffer[BUFFER_SIZE] = {0};
-
-    send(sock, message, strlen(message), 0);
-    printf("Message sent to server\n");
-    read(sock, buffer, BUFFER_SIZE);
-    printf("Message from server: %s\n", buffer);
-
-    //close(sock);
-    return;
+    while (1) {
+        memset(buffer, 0, BUFFER_SIZE);
+        int valread = read(sock, buffer, BUFFER_SIZE);
+        if (valread <= 0) {
+            printf("Disconnected from server.\n");
+            exit(0);
+        }
+        printf("\n%s> ", buffer);
+        fflush(stdout); // clear the line for new input
+    }
+    return NULL;
 }
--- server.c
+++ server.c
@@ -1,67 +1,138 @@
-// Server code in C
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <sys/socket.h>
 #include <arpa/inet.h>
 
-#define PORT 8080
+#define PORT 12345
 #define BUFFER_SIZE 1024
+#define MAX_CLIENTS 10
 
-/*return server socket fd*/
-int server() {
+
+// Global variables to manage clients
+// Note: In a real-world application, you would want to use a more sophisticated data structure
+// to manage clients, such as a linked list or a dynamic array.
+// Here, we use a fixed-size array for simplicity.
+int clients[MAX_CLIENTS];
+int client_count = 0;
+pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// prototypes
+void *handle_client(void *arg);
+void broadcast_message(const char *message, int sender_fd);
+
+int main() {
     int server_fd, new_socket;
     struct sockaddr_in address;
-    int opt = 1;
     int addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE] = {0};
+    
+    pthread_t tid;
 
-    // Creating socket file descriptor
+    // 1. Create socket
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("Socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Attaching socket to the port
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
-
+    // 2. Bind
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
-
-    // Binding the socket
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("Bind failed");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
 
-    // Listening for connections
-    if (listen(server_fd, 3) < 0) {
-        perror("Listen");
-        close(server_fd);
+    // 3. Listen
+    if (listen(server_fd, MAX_CLIENTS) < 0) {
+        perror("Listen failed");
         exit(EXIT_FAILURE);
     }
 
-    printf("Server listening on port %d\n", PORT);
+    printf("Server listening on port %d...\n", PORT);
 
-    // Accepting a client connection
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
-        perror("Accept");
-        close(server_fd);
-        exit(EXIT_FAILURE);
+    // multiple clients
+    while(1){
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) { // on success, return fd for the accepted socket
+            perror("Accept failed");
+            continue;
+        }
+
+        // Add client to list
+        pthread_mutex_lock(&clients_mutex);
+        if (client_count >= MAX_CLIENTS) {
+            char *msg = "Server full. Try again later.\n";
+            send(new_socket, msg, strlen(msg), 0);
+            close(new_socket);
+        } else {
+            clients[client_count++] = new_socket;
+
+            // Broadcast join message
+            char join_msg[BUFFER_SIZE] = {0};
+            snprintf(join_msg, sizeof(join_msg), "Client %d has joined the chatroom.\n", new_socket);
+            broadcast_message(join_msg, -1);  // -1 means send to all
+
+            int *pclient = malloc(sizeof(int));
+            *pclient = new_socket;
+            pthread_create(&tid, NULL, handle_client, pclient);
+            pthread_detach(tid);
+            printf("Client %d connected.\n", new_socket);
+        }
+        pthread_mutex_unlock(&clients_mutex);
+        
     }
-
-    read(new_socket, buffer, BUFFER_SIZE);
-    printf("Message from client: %s\n", buffer);
-    send(new_socket, message, strlen(message), 0);
-    printf("Message sent to client\n");
-
-    close(new_socket);
-  return server_fd;
+    close(server_fd);
     return 0;
+}
+
+void *handle_client(void *arg) {
+    int sock = *(int *)arg;
+    free(arg);
+    char buffer[BUFFER_SIZE] = {0}; // buffer to store incoming messages
+    char message[BUFFER_SIZE+64];
+
+    while (1) {
+        memset(buffer, 0, BUFFER_SIZE); // clear the buffer
+        int valread = read(sock, buffer, BUFFER_SIZE); // read returns 0 if buffer overflows
+        if (valread <= 0) {
+            printf("Client %d disconnected.\n", sock);
+            snprintf(message, sizeof(message), "Client %d has left the chatroom.\n", sock);
+            broadcast_message(message, -1);
+            break;
+        }
+
+        snprintf(message, sizeof(message), "Client %d: %s", sock, buffer);
+        printf("%s", message);
+        broadcast_message(message, sock);
+    }
+
+    // Remove client from list
+    pthread_mutex_lock(&clients_mutex);
+    for (int i = 0; i < client_count; ++i) {
+        if (clients[i] == sock) {
+            clients[i] = clients[client_count - 1]; // Move last client here
+            client_count--;
+            break;
+        }
+    }
+    pthread_mutex_unlock(&clients_mutex);
+
+    close(sock);
+    return NULL;
+}
+
+// using a mutex to protect access to the clients array
+void broadcast_message(const char *message, int sender_fd) {
+    pthread_mutex_lock(&clients_mutex);
+    for (int i = 0; i < client_count; ++i) {
+        if (clients[i] != sender_fd || sender_fd == -1) { // if sender_fd is -1, send to all clients
+            // Send message to all clients except the sender
+            send(clients[i], message, strlen(message), 0);
+        }
+    }
+    pthread_mutex_unlock(&clients_mutex); // lock & unlock the clients array avoid competition between threads
 }
