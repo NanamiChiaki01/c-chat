104a69b1ebc062a640f15094c11efcedd9b390a5
701-extreme <evenlyrl@gmail.com>
2025-08-07 15:05:23 +1200

    allow multiple client

--- server.c
+++ server.c
@@ -2,17 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <netinet/in.h>
 
 #define PORT 12345
 #define BUFFER_SIZE 1024
+#define MAX_CLIENTS 10
+
+void *handle_client(void *arg);
 
 int main() {
     int server_fd, client_fd;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE];
-    const char *reply = "Message received";
+    pthread_t thread_id;
 
     // Create socket
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -31,48 +34,61 @@ int main() {
     }
 
     // Listen
-    if (listen(server_fd, 1) < 0) {
+    if (listen(server_fd, MAX_CLIENTS) < 0) {
         perror("listen failed");
         exit(EXIT_FAILURE);
     }
 
     printf("Server listening on port %d...\n", PORT);
 
-    // Accept client
-    client_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-    if (client_fd < 0) {
-        perror("accept failed");
-        exit(EXIT_FAILURE);
+    // Accept loop
+    while (1) {
+        client_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+        if (client_fd < 0) {
+            perror("accept failed");
+            continue;
+        }
+
+        printf("New client connected (fd = %d)\n", client_fd);
+
+        int *client_sock = malloc(sizeof(int));
+        *client_sock = client_fd;
+
+        if (pthread_create(&thread_id, NULL, handle_client, client_sock) != 0) {
+            perror("pthread_create failed");
+            free(client_sock);
+            continue;
+        }
+
+        pthread_detach(thread_id); // Don't require join
     }
 
-    printf("Client connected.\n");
+    close(server_fd);
+    return 0;
+}
 
-    const char *welcome = "Welcome to the server!";
-    int sent = send(client_fd, welcome, strlen(welcome), 0);
-    if (sent < 0) {
-        perror("send failed");
-    } else {
-        printf("Sent welcome message (%d bytes)\n", sent);
-    }
+// Thread function for handling each client
+void *handle_client(void *arg) {
+    int client_fd = *((int *)arg);
+    free(arg);
 
+    char buffer[BUFFER_SIZE];
+    const char *reply = "Message received";
 
-
-    // Receive and reply loop
     while (1) {
         memset(buffer, 0, BUFFER_SIZE);
         int valread = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
         if (valread <= 0) {
-            printf("Client disconnected.\n");
+            printf("Client (fd=%d) disconnected.\n", client_fd);
             break;
         }
 
-        printf("Client says: %s\n", buffer);
+        buffer[valread] = '\0';
+        printf("Client (fd=%d) says: %s\n", client_fd, buffer);
 
-        // Send reply
         send(client_fd, reply, strlen(reply), 0);
     }
 
     close(client_fd);
-    close(server_fd);
-    return 0;
+    return NULL;
 }
